96cb33333384e1715363b4e23b3557ff41e6accc
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-05-01 16:52:45 +0300

    up

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -6,7 +6,7 @@ const config = {
     },
     development: {
         PORT: 3030,
-        DB_URI: 'mongodb://127.0.0.1:27017/data',
+        DB_URI: 'mongodb+srv://stoilzlatin89:mG5jKz7gWp5sg8qP@data.xloathm.mongodb.net/?retryWrites=true&w=majority',//mongodb://127.0.0.1:27017/data
         SECRET: 'SOMEDEVSECRET',
     }
 };
