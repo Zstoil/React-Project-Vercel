94fd1b4bda49ccb66c32bfc3df53ed8bd1f6c702
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-05-01 18:43:11 +0300

    up

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 
-export const baseUrl =  process.env.NODE_ENV === 'development' 
-? 'http://localhost:3030'//http://localhost:3030
-: 'http://localhost:3030'; // TODO: Add server url when deployed
+// export const baseUrl =  process.env.NODE_ENV === 'development' 
+// ? 'http://localhost:3030'//http://localhost:3030
+// : 'http://localhost:3030'; // TODO: Add server url when deployed
 
-   
+export const baseUrl = 'http://server-node-js-eight.vercel.app'
