d2854180b97dabf42abe5e1e37bfd6a3040a6c46
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-05-01 19:38:11 +0300

    Update baseUrl.js

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -3,4 +3,4 @@
 // ? 'http://localhost:3030'//http://localhost:3030
 // : 'http://localhost:3030'; // TODO: Add server url when deployed
 
-export const baseUrl = 'https://react-project-vercel-server.vercel.app'
+export const baseUrl = 'https://server-node-js-eta.vercel.app'
